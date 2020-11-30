0b174ff9c48471b4ba62281a359aa7d22df9e474
Ubuntu <ubuntu@ip-172-31-55-204.ec2.internal>
2020-11-30 05:35:48 +0000

    Add review list screen

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2020_11_30_044443) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.integer "age"
-    t.text "detail"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
